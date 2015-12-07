a0bfe26135ca332db35e2396338025a5990394a4
datdn1 <datdn1.27@gmail.com>
2015-12-07 23:10:14 +0700

    Add insert/remove function

---
+++ FlickrSearch/OrderDictionary.swift
@@ -0,0 +1,46 @@
+//
+//  OrderDictionary.swift
+//  FlickrSearch
+//
+//  Created by datdn1 on 12/7/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import Foundation
+
+struct OrderedDictionary<KeyType: Hashable, ValueType> {
+    
+    typealias ArrayType = [KeyType]
+    typealias DictionaryType = [KeyType: ValueType]
+    
+    var array = ArrayType()
+    var dictionary = DictionaryType()
+    
+    // mutating can change variable of struct
+    mutating func insert(value: ValueType, forKey key:KeyType, atIndex index:Int) -> ValueType? {
+        var adjustedIndex = index
+        
+        let existingValue = self.dictionary[key]
+        if existingValue != nil {
+            let existingIndex = self.array.indexOf({ (keyElement: KeyType) -> Bool in
+                return keyElement == key
+            })
+            if existingIndex < index {
+                adjustedIndex--
+            }
+            
+            self.array.removeAtIndex(existingIndex!)
+        }
+        self.array.insert(key, atIndex: adjustedIndex)
+        self.dictionary[key] = value
+        return existingValue
+    }
+    
+    mutating func removeAtIndex(index: Int) -> (KeyType, ValueType) {
+        precondition(index < self.array.count, "Index out-of-bound")
+        
+        let key = self.array.removeAtIndex(index)
+        let value = self.dictionary.removeValueForKey(key)!
+        return (key, value)
+    }
+}
